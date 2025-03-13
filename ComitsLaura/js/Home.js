a7455c6a29cdc9b5a879fdc83a9ab6edf31412a4
MarcosPaulo-web <marcos_p_medeiros@estudante.sesisenai.org.br>
2025-03-13 19:17:02 -0300

    AGORA O PERFIL DEIXA O FUNDO (HOME) OPACO, DEIXA ELE MENOS VISIVEL

--- ComitsLaura/js/Home.js
+++ ComitsLaura/js/Home.js
@@ -3,13 +3,16 @@
 function buttonPerfil() {
     // Seleciona o elemento da div que contém o perfil
     let perfilDiv = document.querySelector(".perfilClose");
+    let main = document.querySelector(".main");
 
     // Verifica se a classe 'perfilOpen' já está presente
     if (perfilDiv.classList.contains("perfilOpen")) {
         // Se já estiver aberto, remove a classe para fechar
         perfilDiv.classList.remove("perfilOpen");
+        main.classList.remove("main_Ofuscado")
     } else {
         // Se estiver fechado, adiciona a classe para abrir
         perfilDiv.classList.add("perfilOpen");
+        main.classList.add("main_Ofuscado");
     }
 }
