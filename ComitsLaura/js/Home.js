f9110396133f248c2434672cf04ec9e3509cadfb
MarcosPaulo-web <marcos_p_medeiros@estudante.sesisenai.org.br>
2025-03-14 20:27:53 -0300

    a navbar esta boa

--- ComitsLaura/js/Home.js
+++ ComitsLaura/js/Home.js
@@ -1,4 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', async () => {
+    await carregarNavbar()
+});
 
 function buttonPerfil() {
     // Seleciona o elemento da div que contém o perfil
@@ -7,12 +9,27 @@ function buttonPerfil() {
 
     // Verifica se a classe 'perfilOpen' já está presente
     if (perfilDiv.classList.contains("perfilOpen")) {
-        // Se já estiver aberto, remove a classe para fechar
+        // Se já estiver aberto, remove a classe para fechar O Container
         perfilDiv.classList.remove("perfilOpen");
         main.classList.remove("main_Ofuscado")
     } else {
-        // Se estiver fechado, adiciona a classe para abrir
+        // Se estiver fechado, adiciona a classe para abrir o container 
         perfilDiv.classList.add("perfilOpen");
         main.classList.add("main_Ofuscado");
+    }
+}
+
+
+async function carregarNavbar() {
+    try {
+        const navbarResponse = await fetch('NavbarHome.html');
+        if (navbarResponse.ok) {
+            const navbarHtml = await navbarResponse.text();
+            document.getElementById('navbar-container').innerHTML = navbarHtml;
+        } else {
+            console.error('Erro ao carregar a navbar: Arquivo não encontrado.');
+        }
+    } catch (err) {
+        console.error('Erro ao tentar carregar a navbar:', err);
     }
 }
