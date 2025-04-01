72b082719c500772286c953c4ca93e44c195dbc8
MarcosPaulo-web <marcos_p_medeiros@estudante.sesisenai.org.br>
2025-03-31 22:14:15 -0300

    falta a att 2

--- Algoritmos/03-31/Atividade2.py
+++ Algoritmos/03-31/Atividade2.py
@@ -1,1 +1,14 @@
-# 1-Escrva um programa que lê 3 numeros inteiros e os mostre em ordem crescente
+# 2-Escrva um programa que lê 3 numeros inteiros e os mostre em ordem crescente
+nums = []
+nums.append(int(input("Num 1 :")))
+nums.append(int(input("Num 2 :")))
+nums.append(int(input("Num 3 :")))
+
+for a in range(2):
+    for i in range(2):
+        if nums[i] > nums[i+1]:
+            temp = nums[i+1]     
+            nums[i+1] = nums[i]
+            nums[1] = temp
+
+print(nums)
